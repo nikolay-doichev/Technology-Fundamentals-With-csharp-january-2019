d06c3e26723be219a28c1b2deaa741bb128088d0
Николай Дойчев <nikolay.doichev@gmail.com>
2019-05-10 21:48:31 +0300

    edit

---
+++ 07.AssociativeArrays_Exercise/01. Count Chars in a String/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _02._A_Miner_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var resources = new Dictionary<string, int>();
+
+            string command = Console.ReadLine();
+            
+            while (command!="stop")
+            {
+                string currentResource = command;
+                command = Console.ReadLine();
+                int resourceQuantity = int.Parse(command);
+                if (!resources.ContainsKey(currentResource))
+                {                    
+                    resources.Add(currentResource, resourceQuantity);
+                }
+                else
+                {
+                    resources[currentResource] += resourceQuantity;
+                }
+                command = Console.ReadLine();
+            }
+
+            foreach (var resource in resources)
+            {
+                Console.WriteLine($"{resource.Key} -> {resource.Value}");
+            }
+        }
+    }
+}
